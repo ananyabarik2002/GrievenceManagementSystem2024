993af248aad82761f4ca3d2a8af1ef43d6c919b9
ananyabarik2002 <barikananya741@gmail.com>
2024-11-01 14:30:52 +0530

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes), // Provide router configuration here
+    // Add other environment providers if necessary
+  ],
+}).catch(err => console.error(err));
