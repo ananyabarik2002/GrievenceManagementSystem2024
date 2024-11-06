cd65abd7374be36a2a693e381cb11195459a8c16
Abantika1101 <nandaabantika@gmail.com>
2024-11-06 22:05:14 +0530

    Updated the codes of department component

--- src/app/pages/department/department.component.ts
+++ src/app/pages/department/department.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-department',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './department.component.html',
   styleUrls: ['./department.component.css']
 })
